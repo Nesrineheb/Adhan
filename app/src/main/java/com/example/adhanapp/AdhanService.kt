2589abc518a4ac7139a7b22e66fe96f3ed55714f
racha sai <hr_saidouni@esi.dz>
2021-06-01 00:09:09 +0100

    added adhan

---
+++ app/src/main/java/com/example/adhanapp/AdhanService.kt
@@ -0,0 +1,52 @@
+package com.example.adhanapp
+
+import android.app.Notification
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Intent
+import android.net.Uri
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import com.example.adhanapp.App.Companion.CHANNEL_ID
+
+class AdhanService : Service() {
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        val input = intent.getStringExtra("inputExtra")
+
+
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0, notificationIntent, 0
+        )
+        val notification: Notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setDefaults(0)
+            .setContentTitle("Example Service")
+            .setContentText(input)
+            .setSound(Uri.parse("android.resource://$packageName/${R.raw.azan}"))
+            .setSmallIcon(R.drawable.moon)
+            .setContentIntent(pendingIntent)
+            .build()
+
+
+        startForeground(1, notification)
+
+        //do heavy work on a background thread
+        //stopSelf();
+        return START_NOT_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+    }
+
+    override fun onBind(intent: Intent): IBinder? {
+        return null
+    }
+}
--- app/src/main/java/com/example/adhanapp/MainActivity.kt
+++ app/src/main/java/com/example/adhanapp/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.adhanapp
 
+import android.content.Intent
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+    fun startService(v: View?) {
+        val input = "Prayer Time"
+
+        val  sound: MediaPlayer = MediaPlayer.create(this,R.raw.azan)
+        val serviceIntent = Intent(this, AdhanService::class.java)
+        serviceIntent.putExtra("inputExtra", input)
+
+        ContextCompat.startForegroundService(this, serviceIntent)
+        println("hi!")
+
+        sound.start()
+
+    }
+
+    fun stopService(v: View?) {
+        val serviceIntent = Intent(this, AdhanService::class.java)
+        stopService(serviceIntent)
+
     }
 }
